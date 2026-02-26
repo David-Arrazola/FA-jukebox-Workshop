685fd1eb2271b6a2a0dd7e36bb0c8ebcbee58fc1
David Arrazola <arrazola.dvd@gmail.com>
2026-02-25 20:23:48 -0500

    ADD basic the body-parsing, logging, and error-handling middle ware in App.js

--- app.js
+++ app.js
@@ -1,3 +1,22 @@
 import express from "express";
 const app = express();
 export default app;
+
+app.use(express.json);
+//* BODY-PARSING middleware
+
+app.use("/", (req, res, next) => {
+  console.log(req.method + " " + req.originalUrl);
+
+  next();
+});
+//* LOGGING middleware
+
+//todo ADD tracksRouter
+//todo ADD playlistRouter
+
+app.use("/", (err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).send("Something went wrong");
+});
