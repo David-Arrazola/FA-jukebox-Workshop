94f7269c125fa70d8b0f2c1a26edb1a85821f6fd
David Arrazola <arrazola.dvd@gmail.com>
2026-02-25 19:01:28 -0500

    ADD three tables in schema.sql; playlists, tracks, & playlists_tracks

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,19 @@
--- TODO
+CREATE TABLE playlists(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    description TEXT NOT NULL 
+);
+
+CREATE TABLE tracks(
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    duration_ms INT NOT NULL
+); 
+
+CREATE TABLE playlists_tracks(
+    PRIMARY KEY (playlists_id, tracks_id)
+    playlist_id INT NOT NULL REFERENCES playlists(id) ON DELETE CASCADE,
+    track_id INT NOT NULL REFERENCES tracks(id) ON DELETE CASCADE,
+);
+--* "PRIMARY KEY (playlists_id, tracks_id)" = id tuple identifies the entry row. 
+--* Per PRIMARY KEY rules, each entry is UNIQUE. Cant repeat.
