1c414f26a4ff05c6d9194ec29057595338a76b7e
David Arrazola <arrazola.dvd@gmail.com>
2026-02-26 08:59:34 -0500

    ADD router middleware that will call the query function, createPlaylistTrack, which makes a new entry in the junction table with the specified playlistId and trackId

--- api/playlists.js
+++ api/playlists.js
@@ -1,8 +1,16 @@
 import express from "express";
-import { getAllPlaylists } from "#db/queries/playlists";
+import { getAllPlaylists, getTracksInPlaylist } from "#db/queries/playlists";
+import { createPlaylistTrack } from "#db/queries/playlists_tracks";
 
 const playlistRouter = express.Router();
 export default playlistRouter;
+
+playlistRouter.get("/:id/tracks", async (req, res) => {
+  const { id } = req.params;
+  const tracksInPlaylist = await getTracksInPlaylist(id);
+
+  res.send(tracksInPlaylist);
+});
 
 playlistRouter.get("/", async (req, res) => {
   try {
@@ -12,4 +20,17 @@ playlistRouter.get("/", async (req, res) => {
   } catch (e) {
     console.error(e);
   }
+});
+
+playlistRouter.post("/:id/tracks", async (req, res) => {
+  if (!req.body) return res.status(400).send("Request body is required.");
+
+  const playlistId = Number(req.params.id);
+  const { trackId } = req.body;
+
+  if (!trackId) return res.status(400).send("TrackId is needed");
+
+  const addedTrack = await createPlaylistTrack(playlistId, trackId);
+
+  res.status(201).send(addedTrack);
 });
